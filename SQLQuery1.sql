bcaa8b340b6217ec051fa201cde4f5f601bf3964
sahiba <sahiba1214@gmail.com>
2021-12-14 20:08:04 +0530

    [Sahiba1214] Createtable

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,3 +2,7 @@
 create database AddressBookService;
 
 use AddressBookService;
+
+/*UC2*/
+create table AddressBookTable(id int identity (1,1) primary key, FirstName varchar(30), LastName varchar(30), Address varchar(40), City varchar(20), State varchar(20), Zip varchar(6), Phone varchar(16), Email varchar(40));
+exec sp_help AddressBookTable;
